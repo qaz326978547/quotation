70217781e6fc20018dc44d5e4387e5bd2e5dd868
Bean <qaz326978547@gmail.com>
2024-02-07 18:11:59 +0800

    [add] bootstrap scss環境 , SEO

--- app.vue
+++ app.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    <NuxtWelcome />
+
+  <div class="container ">
+    <h1 class="fw-bold mt-5">報價單產生器</h1>
+    <form class="p-4 ">
+      <h2>主要資料</h2>
+      <div class="mb-3">
+        <label for="companyName" class="form-label">公司名稱</label>
+        <input type="text" class="form-control" id="companyName" placeholder="請輸入公司名稱">
+      </div>
+      <div class="row mb-30">
+        <div class="mb-3 col">
+        <label for="companyAddress" class="form-label">公司地址</label>
+        <input type="text" class="form-control" id="companyAddress" placeholder="請輸入公司地址">
+      </div>
+      <div class="mb-3 col">
+        <label for="companyTel" class="form-label">公司電話</label>
+        <input type="text" class="form-control" id="companyTel" placeholder="請輸入公司電話">
+      </div>
+      </div>
+     
+   
+    </form>
   </div>
+
 </template>
+
+<script setup lang="ts">
+
+useSeoMeta({
+  title: '報價單產生器', // 頁面標題
+  ogTitle: '報價單產生器', // Open Graph 標題
+  description: '這是一個報價單產生器網頁，你可以在這裡快速產生報價單', // 頁面描述
+  ogDescription: '這是一個報價單產生器網頁，你可以在這裡快速產生報價單', // Open Graph 描述
+  ogImage: '/og-image.jpg', // Open Graph 圖片
+})
+</script>
+
+<style lang="scss">
+
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css:[
+    '@/assets/scss/app.scss'
+  ],
   devtools: { enabled: true }
 })
